9644c9d57244c94c2766402d806ed75cb76bf4b9
squirrel1021 <ouchenyin@163.com>
2017-06-18 22:33:11 +0800

    添加注册界面

--- app/src/main/java/com/july/pigeon/ui/activity/login/RegisterActivity.java
+++ app/src/main/java/com/july/pigeon/ui/activity/login/RegisterActivity.java
@@ -1,53 +1,40 @@
 package com.july.pigeon.ui.activity.login;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
-import android.widget.TextView;
 
 import com.july.pigeon.R;
 import com.july.pigeon.ui.activity.BaseActivity;
 import com.july.pigeon.ui.activity.main.HomeActivity;
-import com.july.pigeon.ui.activity.main.WelcomeActivity;
 import com.july.pigeon.util.ActivityStartUtil;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
- * Created by Administrator on 2017/6/17 0017.
+ * Created by Administrator on 2017/6/18 0018.
  */
 
-public class LoginActivity extends Activity implements View.OnClickListener {
-    private Button btn;
-
+public class RegisterActivity extends Activity implements View.OnClickListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main);
+        setContentView(R.layout.register_activity);
         initView();
     }
 
     private void initView() {
-        btn = (Button) findViewById(R.id.loginBtn);
-        btn.setOnClickListener(this);
-        ((TextView)findViewById(R.id.regiest)).setOnClickListener(this);
+        ((Button)findViewById(R.id.yzmBtn)).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.loginBtn:
-                ActivityStartUtil.start(this,HomeActivity.class);
-                finish();
-                break;
-            case R.id.regiest:
-                ActivityStartUtil.start(this,RegisterActivity.class);
+            case R.id.yzmBtn:
+                ActivityStartUtil.start(this,RegisterFormal.class);
                 finish();
                 break;
             default:
---
+++ app/src/main/java/com/july/pigeon/ui/activity/login/RegisterFormal.java
@@ -0,0 +1,19 @@
+package com.july.pigeon.ui.activity.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.july.pigeon.R;
+
+/**
+ * Created by Administrator on 2017/6/18 0018.
+ */
+
+public class RegisterFormal extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_formal);
+    }
+}
