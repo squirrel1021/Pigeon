197e68e5858803851c8dc2dad31b70d88eea0d4a
squirrel1021 <ouchenyin@163.com>
2017-06-21 23:40:46 +0800

    增加选择图片功能

--- app/src/main/java/com/july/pigeon/ui/activity/BaseActivity.java
+++ app/src/main/java/com/july/pigeon/ui/activity/BaseActivity.java
@@ -1,20 +1,30 @@
 package com.july.pigeon.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.andsync.xpermission.XPermissionUtils;
+
 /**
  * Created by ANDROID on 2017/6/7.
  */
 
-public class BaseActivity extends FragmentActivity{
+public class BaseActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        XPermissionUtils.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/com/july/pigeon/ui/activity/login/ForgetPassWordActivity.java
+++ app/src/main/java/com/july/pigeon/ui/activity/login/ForgetPassWordActivity.java
@@ -1,16 +1,31 @@
 package com.july.pigeon.ui.activity.login;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.andsync.xpermission.XPermissionUtils;
 import com.july.pigeon.R;
 import com.july.pigeon.ui.activity.BaseActivity;
+import com.pizidea.imagepicker.AndroidImagePicker;
+import com.pizidea.imagepicker.bean.ImageItem;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/6/19 0019.
@@ -21,6 +36,7 @@ public class ForgetPassWordActivity extends BaseActivity {
     public TextView title;
     @BindView(R.id.left_tv)
     public TextView leftTv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +49,47 @@ public class ForgetPassWordActivity extends BaseActivity {
         title.setText("忘记密码");
         leftTv.setText("取消");
         leftTv.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.next_step)
+    public void nextStep() {
+        XPermissionUtils.requestPermissions(this, 1, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                new XPermissionUtils.OnPermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        selectImage();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(final String[] deniedPermissions, boolean alwaysDenied) {
+                        Toast.makeText(ForgetPassWordActivity.this, "获取权限失败", Toast.LENGTH_SHORT).show();
+                        if (alwaysDenied) { // 拒绝后不再询问 -> 提示跳转到设置
+                            new AlertDialog.Builder(ForgetPassWordActivity.this).setTitle("温馨提示")
+                                    .setMessage("我们需要读写权限才能正常使用该功能")
+                                    .setNegativeButton("取消", null)
+                                    .setPositiveButton("打开权限", new DialogInterface.OnClickListener() {
+                                        @RequiresApi(api = Build.VERSION_CODES.M)
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+//                                            XPermissionUtils.requestPermissionsAgain(ForgetPassWordActivity.this, deniedPermissions,
+//                                                    1);
+                                            Uri packageURI = Uri.parse("package:" + ForgetPassWordActivity.this.getPackageName());
+                                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+                                            startActivity(intent);
+                                        }
+                                    })
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+    private void selectImage() {
+        AndroidImagePicker.getInstance().pickMulti(ForgetPassWordActivity.this, true, new AndroidImagePicker.OnImagePickCompleteListener() {
+            @Override
+            public void onImagePickComplete(List<ImageItem> items) {
+
+            }
+        });
     }
 }
